ca0a75c63fe49183bd0dacc96f564d928ca129da
Matt Graham <mpgraham917@gmail.com>
2024-06-05 15:36:20 -0400

    Base mechanics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,181 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import Carousel from './components/carousel'
+import { sixLetterWords } from './constants'
+import { useCallback, useMemo, useState } from 'react'
+import keyIcon from './assets/key-icon.svg'
+import { shuffle } from './utils'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [selectedLetters, setSelectedLetters] = useState<string[]>(() =>
+        new Array(6).fill(''),
+    )
+    const [correctRows, setCorrectRows] = useState<string[]>(() =>
+        new Array(6).fill(''),
+    )
+    const [incorrectRows, setIncorrectRows] = useState(() => {
+        const rows: string[][] = []
+        for (let i = 0; i < 6; i++) {
+            rows.push([])
+        }
+        return rows
+    })
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    /**
+     * Select a few random words
+     */
+    const randomWords = useMemo(() => {
+        const randomWords = []
+        for (let i = 0; i < 10; i++) {
+            const randomIndex = Math.floor(
+                Math.random() * sixLetterWords.length,
+            )
+            randomWords.push(sixLetterWords[randomIndex])
+        }
+        return randomWords
+    }, [])
+
+    /**
+     * Select a target word from the array of random words
+     */
+    const targetWord = useMemo(
+        () => randomWords[Math.floor(Math.random() * randomWords.length)],
+        [randomWords],
+    )
+
+    const letterRows = useMemo(() => {
+        /**
+         * Create rows from random words (use sets so we can dedupe)
+         */
+        const rowSets = []
+        for (let rowIndex = 0; rowIndex < 6; rowIndex++) {
+            const rowSet = new Set<string>()
+            randomWords.forEach((word) => {
+                const letter = word.charAt(rowIndex)
+                rowSet.add(letter)
+            })
+            rowSets.push(rowSet)
+        }
+
+        /**
+         * Create row arrays
+         */
+        const rows = rowSets.map((rowSet) => [...rowSet])
+        rows.forEach((row) => shuffle(row))
+        return rows
+    }, [randomWords])
+
+    const handleLetterChange = useCallback((row: number, letter: string) => {
+        setSelectedLetters((prev) => {
+            const nextSelectedLetters: string[] = []
+            for (let i = 0; i < 6; i++) {
+                if (row === i) {
+                    nextSelectedLetters.push(letter)
+                } else {
+                    nextSelectedLetters.push(prev[i])
+                }
+            }
+            return nextSelectedLetters
+        })
+    }, [])
+
+    const handleTestClick = useCallback(() => {
+        selectedLetters.forEach((letter, i) => {
+            if (letter === targetWord.charAt(i)) {
+                setCorrectRows((prev) =>
+                    prev.map((existingLetter, index) =>
+                        i === index ? letter : existingLetter,
+                    ),
+                )
+            } else {
+                setIncorrectRows((prev) => {
+                    const nextIncorrectRows: string[][] = []
+                    for (let index = 0; index < 6; index++) {
+                        if (i === index) {
+                            const nextIncorrectRow: string[] = [
+                                ...new Set([...prev[index], letter]),
+                            ]
+                            nextIncorrectRows.push(nextIncorrectRow)
+                        } else {
+                            nextIncorrectRows.push(prev[index])
+                        }
+                    }
+                    return nextIncorrectRows
+                })
+            }
+        })
+    }, [selectedLetters, targetWord])
+
+    const disableTestButton = useMemo(
+        () => selectedLetters.some((letter) => !letter),
+        [selectedLetters],
+    )
+
+    console.log(selectedLetters)
+    return (
+        <>
+            <p>cryptle</p>
+            {/* <p>word: {targetWord}</p> */}
+            <p>
+                selected:{' '}
+                {selectedLetters
+                    .map((letter) => (letter === '' ? '?' : letter))
+                    .join(' ')}
+            </p>
+            <div className="carousels">
+                <Carousel
+                    rowIndex={0}
+                    letters={letterRows[0]}
+                    correct={correctRows[0]}
+                    incorrect={incorrectRows[0]}
+                    onChangeLetter={handleLetterChange}
+                />
+                <Carousel
+                    rowIndex={1}
+                    letters={letterRows[1]}
+                    correct={correctRows[1]}
+                    incorrect={incorrectRows[1]}
+                    onChangeLetter={handleLetterChange}
+                />
+                <Carousel
+                    rowIndex={2}
+                    letters={letterRows[2]}
+                    correct={correctRows[2]}
+                    incorrect={incorrectRows[2]}
+                    onChangeLetter={handleLetterChange}
+                />
+                <Carousel
+                    rowIndex={3}
+                    letters={letterRows[3]}
+                    correct={correctRows[3]}
+                    incorrect={incorrectRows[3]}
+                    onChangeLetter={handleLetterChange}
+                />
+                <Carousel
+                    rowIndex={4}
+                    letters={letterRows[4]}
+                    correct={correctRows[4]}
+                    incorrect={incorrectRows[4]}
+                    onChangeLetter={handleLetterChange}
+                />
+                <Carousel
+                    rowIndex={5}
+                    letters={letterRows[5]}
+                    correct={correctRows[5]}
+                    incorrect={incorrectRows[5]}
+                    onChangeLetter={handleLetterChange}
+                />
+                <div className="center" />
+            </div>
+            <button
+                className="test-button"
+                type="button"
+                onClick={handleTestClick}
+                disabled={disableTestButton}
+            >
+                <img src={keyIcon} alt="key" />
+            </button>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,169 @@
+import { useCallback, useEffect, useState } from 'react'
+import './Carousel.scss'
+
+export interface CarouselProps {
+    rowIndex: number
+    letters: string[]
+    correct: string
+    incorrect: string[]
+    onChangeLetter: (row: number, letter: string) => void
+}
+
+const letterSize = 64
+const numLettersInContainerViewport = 10
+
+export default function Carousel({
+    rowIndex,
+    letters,
+    correct,
+    incorrect,
+    onChangeLetter,
+}: CarouselProps) {
+    const [ref, setRef] = useState<HTMLDivElement | null>(null)
+
+    const handleCarouselUnsettled = useCallback(() => {
+        console.log('unsettled')
+        onChangeLetter(rowIndex, '')
+    }, [onChangeLetter, rowIndex])
+
+    const handleCarouselSettled = useCallback(
+        (x: number) => {
+            console.log('settled')
+            const letterIndex = Math.floor(
+                -x / letterSize + numLettersInContainerViewport / 2,
+            )
+            const letter = letters[letterIndex]
+            console.log('letterIndex=', letterIndex)
+            console.log('letter=', letter)
+            onChangeLetter(rowIndex, letter)
+        },
+        [onChangeLetter, rowIndex, ref, letters],
+    )
+
+    useEffect(() => {
+        if (!ref) return // Make sure ref exists
+
+        /**
+         * Init variables
+         */
+        const letterMiddle = letterSize / 2
+        const drag = 0.9
+        let holding = false
+        let posX = (letterSize * numLettersInContainerViewport) / 2
+        let velX = 0
+        let mouseX = 0
+        let lastMouseX = 0
+        let settled = false
+
+        /*
+         * Create listener functions
+         */
+        const mouseDown = (event: MouseEvent) => {
+            holding = true
+            mouseX = event.pageX
+            if (settled) {
+                settled = false
+                handleCarouselUnsettled()
+            }
+        }
+        const mouseMove = (event: MouseEvent) => {
+            mouseX = event.pageX
+        }
+        const mouseUp = () => {
+            holding = false
+        }
+
+        /*
+         * Attach listeners
+         */
+        ref.addEventListener('mousedown', mouseDown)
+        document.addEventListener('mousemove', mouseMove)
+        document.addEventListener('mouseup', mouseUp)
+
+        /*
+         * Loop
+         */
+        let frame: number
+        function loop() {
+            frame = requestAnimationFrame(loop)
+
+            if (!ref) return // For type safety
+
+            const deltaMouseX = mouseX - lastMouseX
+            const deltaLetterMiddle =
+                letterMiddle -
+                (posX > 0
+                    ? posX % letterSize
+                    : letterSize + (posX % letterSize))
+            if (holding) {
+                velX = deltaMouseX
+            } else {
+                if (Math.abs(deltaLetterMiddle) > 1) {
+                    velX +=
+                        (deltaLetterMiddle / Math.abs(deltaLetterMiddle)) * 0.2
+                }
+
+                velX *= drag
+            }
+
+            if (!settled && !holding && Math.abs(velX) < 0.01) {
+                settled = true
+                handleCarouselSettled(posX)
+            }
+
+            posX += velX // Apply velocity
+            const maxPosX = Math.floor(
+                (numLettersInContainerViewport / 2) * letterSize - 0.01,
+            )
+            const minPosX =
+                -ref.clientWidth +
+                (numLettersInContainerViewport / 2) * letterSize +
+                0.01
+            posX = Math.min(maxPosX, Math.max(posX, minPosX)) // clamp position to bounds
+
+            ref.style.left = `${posX}px`
+
+            // Setup for next loop iteration
+            lastMouseX = mouseX
+        }
+        loop()
+
+        return () => {
+            ref.removeEventListener('mousedown', mouseDown)
+            document.removeEventListener('mousemove', mouseMove)
+            document.removeEventListener('mouseup', mouseUp)
+            cancelAnimationFrame(frame)
+        }
+    }, [ref, handleCarouselSettled, handleCarouselUnsettled])
+
+    return (
+        <div
+            className="carousel-container"
+            style={{
+                fontSize: letterSize,
+                width: `${numLettersInContainerViewport}em`,
+            }}
+        >
+            <div className="carousel" ref={setRef}>
+                {letters.map((letter, index) => {
+                    let status = 'unknown'
+                    if (letter === correct) {
+                        status = 'correct'
+                    } else if (incorrect.includes(letter)) {
+                        status = 'incorrect'
+                    }
+                    return (
+                        <div
+                            className="letter"
+                            key={index}
+                            data-status={status}
+                            data-row-index={rowIndex}
+                        >
+                            <div className="inner">{letter}</div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
