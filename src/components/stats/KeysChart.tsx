8b08a4985be5fbe7e23400a30c305efabd37c8a2
Matt G <mpgraham917@gmail.com>
2024-06-27 15:01:02 -0700

    Add title to unclear label

--- src/components/stats/KeysChart.tsx
+++ src/components/stats/KeysChart.tsx
@@ -8,7 +8,7 @@ export interface KeysChartProps {
 }
 
 export default function KeysChart({ targetDate, data }: KeysChartProps) {
-    const bars: { label: string; height: number; today: boolean }[] = useMemo(() => {
+    const bars: { label: string; title?: string; height: number; today: boolean }[] = useMemo(() => {
         const counts: number[] = new Array(6).fill(0)
         Object.values(data).forEach(({ keysUsed }) => {
             if (keysUsed === -1) {
@@ -27,7 +27,12 @@ export default function KeysChart({ targetDate, data }: KeysChartProps) {
             { label: '3', height: max > 0 ? (counts[2] / max) * 100 : 0, today: keysUsedToday === 3 },
             { label: '4', height: max > 0 ? (counts[3] / max) * 100 : 0, today: keysUsedToday === 4 },
             { label: '5', height: max > 0 ? (counts[4] / max) * 100 : 0, today: keysUsedToday === 5 },
-            { label: 'NC', height: max > 0 ? (counts[5] / max) * 100 : 0, today: keysUsedToday === -1 },
+            {
+                label: 'NC',
+                title: 'Not Completed',
+                height: max > 0 ? (counts[5] / max) * 100 : 0,
+                today: keysUsedToday === -1,
+            },
         ]
     }, [data, targetDate])
 
@@ -35,11 +40,13 @@ export default function KeysChart({ targetDate, data }: KeysChartProps) {
         <>
             <div className="keys-chart-title">Keys Used</div>
             <div className="keys-chart">
-                {bars.map(({ label, height, today }, index) => {
+                {bars.map(({ label, title, height, today }, index) => {
                     return (
                         <div key={index} className="column">
                             <div className={combineClasses('bar', today && 'today')} style={{ height }} />
-                            <div className="label">{label}</div>
+                            <div className="label" title={title}>
+                                {label}
+                            </div>
                         </div>
                     )
                 })}
